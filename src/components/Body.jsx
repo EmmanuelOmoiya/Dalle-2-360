23f9583c5b522351dcf51365412af9d58b13a3b3
EmmanuelOmoiya <emmanuelomoiya6@gmail.com>
2023-03-11 14:21:41 +0100

    Ruuse

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,13 +7,7 @@ export default function Body() {
     return (
         <main className="w-full bg-black md:pl-[13.05rem]">
             <div className="absolute bg-gray-500 bg-opacity-50 backdrop-blur-sm w-full h-full animate-pulse"></div>
-            {
-                imageUrl === "https://i.ibb.co/Ph2Mszw/image.png"
-                ?
-                <iframe className="h-[100vh] w-full mt-[4.5rem] md:m-0 relative" allowFullScreen={true} allow="accelerometer; magnetometer; gyroscope" src="https://i.ibb.co/Ph2Mszw/image.png" ></iframe>
-                :
                 <EquirectangularImage />
-            }
         </main>
     )
 }
